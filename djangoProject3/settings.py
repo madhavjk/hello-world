9b02c5567563e99a62174c8e193256119d0212ea
Madhav Sharma <msharma043510@gmail.com>
2021-01-24 16:26:55 +0530

    initial commit

--- djangoProject3/settings.py
+++ djangoProject3/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig',
+    'posts.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,8 +55,8 @@ ROOT_URLCONF = 'djangoProject3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pages/views.py
+++
@@ -1,5 +0,0 @@
-# Create your views here.
-from django.http import HttpResponse
-
-def homePageView(request):
-    return HttpResponse('Hello,World!')
